9a5a70ead905dea15db026dfd45f249c37dea1fd
Helani Seekkubadu <seekkub-se19022@stu.kln.ac.lk>
2026-05-21 08:02:23 +1000

    small update

--- cli_uni_app.py
+++ cli_uni_app.py
@@ -9,7 +9,7 @@ def help():
     print("X = Exit")
 
 def read_choice():
-    return input("University System: (A)dmin, (S)tudent, or Exit(X) : ").strip().upper()
+    return input("University System: (A)dmin, (S)tudent, or (X)Exit : ").strip().upper()
 
 def main():
     try:
