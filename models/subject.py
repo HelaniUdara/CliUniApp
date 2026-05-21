6c4add6e84127349117c4585b0432cf5c3a398c1
jtechwiz <275290931+jayatidave0@users.noreply.github.com>
2026-05-21 14:25:12 +1000

    fixed grading logic to prevent invalid marks defaulting to HD

--- models/subject.py
+++ models/subject.py
@@ -7,16 +7,16 @@ class Subject:
         self.grade = self._get_grade()
 
     def _get_grade(self):
-        if self.mark < 50:
-            return "Z"          
-        elif self.mark >= 50 and self.mark < 65:
-            return "P"         
+        if self.mark >= 85:
+            return "HD"          
+        elif self.mark >= 75 and self.mark < 85:
+            return "D"         
         elif self.mark >= 65 and self.mark < 75:
             return "C"         
-        elif self.mark >= 75 and self.mark < 85:
-            return "D"          
+        elif self.mark >= 50 and self.mark < 65:
+            return "P"          
         else:
-            return "HD"         
+            return "Z"         
 
     def __str__(self):
         return f"(Subject = {self.id}, mark = {self.mark}, grade = {self.grade})"
